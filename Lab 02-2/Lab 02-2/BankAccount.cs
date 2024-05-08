32ad4ccb1d7a08b661f9a27ba6a328572ac57991
Kushani Tharushika <135972088+KushaniTharushika@users.noreply.github.com>
2024-05-08 23:23:17 +0530

    Lab 02-2
    Lab sheet 02 Question 2

---
+++ Lab 02-2/Lab 02-2/BankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02_2
+{
+    internal class BankAccount
+
+    {
+        public string AccountNumber { get; private set; }
+        public double Balance { get; private set; }
+       
+    }
+}
---
+++ Lab 02-2/Lab 02-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+
+            Console.Write("Enter the account number: ");
+            account.AccountNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the deposit amount: ");
+            double amount = double.Parse(Console.ReadLine());
+
+            account.deposit(amount);
+
+            Console.WriteLine("Account Number: " + account.AccountNumber);
+            Console.WriteLine("Updated Balance: " + account.Balance);
+
+            Console.ReadLine();
+        }
+    }
+}
