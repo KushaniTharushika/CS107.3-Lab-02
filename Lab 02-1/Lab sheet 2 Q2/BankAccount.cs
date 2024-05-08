b7c2843e1169b58b2e755fa19c10d80a664075ff
Kushani Tharushika <135972088+KushaniTharushika@users.noreply.github.com>
2024-05-08 23:22:17 +0530

    Lab 02-1
    Lab sheet 02 Question 1

---
+++ Lab 02-1/Lab sheet 2 Q2/BankAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_sheet_2_Q2
+{
+    internal class BankAccount
+    {
+        public int AccountNumber {  get; set; }
+        public double Balance { get; set; }
+
+        public void deposit(double amount)
+        {
+            Balance = Balance + amount;
+        }
+    }
+}
