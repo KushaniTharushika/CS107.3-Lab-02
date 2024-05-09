bd6ec3fb3d968b68c3ff5b0251a8be8cc44e097f
Kushani Tharushika <135972088+KushaniTharushika@users.noreply.github.com>
2024-05-09 20:24:20 +0530

    Lab 02-3
    lab sheet 02 question 3

---
+++ Lab 02-3/Lab 02-3/Temperture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02_3
+{
+    internal class Temperture
+    {
+            private double[] tempertures; 
+
+            public Temperture()
+            {
+                tempertures = new double[7];
+            }
+
+            public void InputTempertures()
+            {
+                for (int i = 0; i < tempertures.Length; i++)
+                {
+                    Console.Write($"Enter temperature for day {i + 1}: ");
+                    tempertures[i] = double.Parse(Console.ReadLine());
+                }
+            }
+
+            public void DisplayWeeklyReport()
+            {
+                Console.WriteLine("\nWeekly Temperature Report:");
+                for (int i = 0; i < tempertures.Length; i++)
+                {
+                    Console.WriteLine($"Day {i + 1}: {tempertures[i]} °C");
+                }
+            }
+    }
+}
