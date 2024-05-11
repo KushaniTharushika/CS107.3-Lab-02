b32dc9ca09a6a9e3f2e9d96e67fe569c04a31c07
Kushani Tharushika <135972088+KushaniTharushika@users.noreply.github.com>
2024-05-11 23:22:30 +0530

    Lab 02-5
    Lab sheet 02 Question 5

---
+++ Lab 02-5/Lab 02-5/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02_5
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool Available { get; set; }
+        
+        public void LibraryBook(string title, string author )
+        {
+            Title = title;
+            Author = author;
+            Available = true;
+        }
+
+        public void BorrowBook() 
+        {
+            if (Available)
+            {
+                Available = false;
+
+                Console.WriteLine($"Book '{Title}' by {Author} has been borrowed.");
+            }
+            else
+            {
+                Console.WriteLine($"Book '{Title}' by {Author} is not available for borrowing.");
+            }
+        }
+    }
+}
---
+++ Lab 02-5/Lab 02-5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>
+            {
+                new Book("Madol duwa", "Martin Wikramasinghe"),
+                new Book("Robinson Cruseo", "Daniel Defoe"),
+                new Book("1984", "George Orwell")
+            };
+
+            Console.WriteLine("Attempting to borrow books...");
+
+            foreach (var book in books)
+            {
+                book.BorrowBook();
+            }
+
+            
+            Console.WriteLine("\nLibrary Status after borrowing:");
+
+            foreach (var book in books)
+            {
+                Console.WriteLine($"Book '{book.Title}' by {book.Author} is {(book.Available ? "available" : "not available")}");
+            }
+        }
+    }
+}
